882d4bd87773030d6a8a690e5de390578914f5d3
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-01-25 11:32:45 -0700

    you can change the grid's size

--- main.js
+++ main.js
@@ -3,23 +3,31 @@ const container = document.querySelector('#container');
 const content = document.createElement('div');
 content.classList.add('grid');
 let gridSize = 16;
+createGrid()
 
 let size = document.querySelector('#size')
     size.onclick = function(){
-        sizeNum = prompt("Select your grid size.")
-        if (sizeNum < 100 && sizeNum > 16) {
+        sizeNum = prompt("Please enter a grid size between 16 and 100.")
+        if (sizeNum <= 100 && sizeNum > 16) {
             gridSize = sizeNum
+            createGrid()
         }
 
     }
 
+function createGrid() {
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('gridSquare');
-    content.appendChild(gridSquare);
-    hover(gridSquare)
-}
+    content.innerHTML = ''
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquare');
+        gridSquare.style.width = `calc(100%/ ${gridSize})`;
+        gridSquare.style.height = `calc(100%/ ${gridSize})`;
+        content.appendChild(gridSquare);
+        hover(gridSquare)
+        }
+}   
 
 
 
