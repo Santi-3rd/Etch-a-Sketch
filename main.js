720cd0af0889d27711494c71557e4cb523eed895
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-01-25 12:14:28 -0700

    rainbowmode checkbox is bugged

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const content = document.createElement('div');
 content.classList.add('grid');
 let gridSize = 16;
 let rainbowMode = document.querySelector('#rainbow');
-console.log(rainbowMode)
 createGrid()
 
 let size = document.querySelector('#size')
@@ -33,8 +32,14 @@ function createGrid() {
 
 
 function Painter(gridSquare){
+    console.log(rainbowMode.checked)
     if (rainbowMode.checked){
-        
+        gridSquare.addEventListener("mouseover", event => {
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+            gridSquare.style.backgroundColor =  + r + g + b;
+        });
     } else {
         gridSquare.addEventListener("mouseover", event => {
             gridSquare.style.backgroundColor = "black"
