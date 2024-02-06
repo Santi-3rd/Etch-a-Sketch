287451877e2fd6238f97e8a0cc30b23ae39268ce
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-02-06 15:48:29 -0700

    changed shader mode logic and improved createGrid performance

--- main.js
+++ main.js
@@ -10,16 +10,31 @@ createGrid()
 
 
 const slider = document.getElementById('numberSlider');
+const output = document.getElementById('selectedNumber');
+let sliderValueChanged = false;
 
 slider.oninput = function() {
     const selectedNumber = this.value;
+    output.textContent = `${selectedNumber} x ${selectedNumber}`;
     gridSize = selectedNumber
-    createGrid();
+    sliderValueChanged = true;
 };
+slider.onmouseup = function() {
+    if (sliderValueChanged) {
+        createGrid();
+        sliderValueChanged = false;
+    }
+};
+
 
 let clear = document.querySelector('#clear')
     clear.onclick = function(){
         createGrid()
+        content.classList.add('shake-animation');
+    
+        content.addEventListener('animationend', function() {
+            content.classList.remove('shake-animation');
+        });
     }
 
 function createGrid() {
@@ -38,6 +53,7 @@ function createGrid() {
 
 function painter(gridSquare){
     let alpha = 0.1
+    let whiteRgb = 255
     gridSquare.addEventListener("mouseover", event => {
         if (rainbowMode.checked){ 
         let r = Math.floor(Math.random() * 256);
@@ -45,17 +61,20 @@ function painter(gridSquare){
         let b = Math.floor(Math.random() * 256);
         gridSquare.style.backgroundColor = shaderMode.checked ? "rgba(" + r + "," + g + "," + b + "," + alpha + ")" : "rgb(" + r + "," + g + "," + b + ")";
         } else {
-            gridSquare.style.backgroundColor = shaderMode.checked ? "rgba(0, 0, 0, " + alpha + ")" : "rgb(0, 0, 0)";
+            if (shaderMode.checked) {
+                whiteRgb -= 25; // Subtracting from whiteRgb to make color darker
+                let colorValue = whiteRgb < 0 ? 0 : whiteRgb; // Ensure colorValue is within range [0, 255]
+                gridSquare.style.backgroundColor = "rgba(" + colorValue + "," + colorValue + "," + colorValue + ")";
+            } else {
+                whiteRgb = 255; // Reset whiteRgb to maximum value
+                gridSquare.style.backgroundColor = "rgb(0, 0, 0)"; // Set color to black
+            }
         }
-
         if (shaderMode.checked && alpha < 1) {
             alpha += 0.1
         }
     });
 }
-
-
-
 
 container.appendChild(content);
 
